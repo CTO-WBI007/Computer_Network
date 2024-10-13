1a2215c903d9586d150d3214db4ed8f2b2387922
James Bond <120696482+CTO-WBI007@users.noreply.github.com>
2024-10-13 13:54:52 +0800

    Update client.py

--- UDP/client.py
+++ UDP/client.py
@@ -3,27 +3,28 @@ import time
 import random
 import string
 
+# use random function to generate strings
 def generate_random_string(length=10):
     characters = string.ascii_letters + string.digits  
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
 
 def start_client():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create a socket
 
     
     for i in range (0, 100):
         message = generate_random_string()
         print(message)
 
-        client_socket.sendto(message.encode(), ('localhost', 8080))
+        client_socket.sendto(message.encode(), ('localhost', 8080)) # encode and snending
 
-        result = client_socket.recv(1024)
+        result = client_socket.recv(1024) # accept and receive
     
         print(result.decode())
 
-        time.sleep(0.1)
+        time.sleep(0.1) # set interval between each sending to control the band width
     
-    client_socket.close()
+    client_socket.close() # turn the socket down
 
 start_client()
