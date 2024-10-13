7f85f7790058a6c5730b3d4883f18a4351d2d3c4
James Bond <120696482+CTO-WBI007@users.noreply.github.com>
2024-10-13 13:58:04 +0800

    Update server.py

--- UDP/server.py
+++ UDP/server.py
@@ -1,11 +1,11 @@
 import socket
 
 def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create the socket
 
-    server_socket.bind(('0.0.0.0', 8080))
+    server_socket.bind(('0.0.0.0', 8080)) # binding
 
-    server_socket.settimeout(30)
+    server_socket.settimeout(30) # set timeout to shut the server down
 
     print('Ready to receive')
 
@@ -14,14 +14,14 @@ def start_server():
     while cnt < 100:
         try:
             message, addr = server_socket.recvfrom(1024)
-            if isinstance(message, bytes):
-                result = message.decode().upper()
+            if isinstance(message, bytes): # judge whether the message is coded in bytes
+                result = message.decode().upper() # transfer to upper case
                 cnt += 1
                 server_socket.sendto(result.encode(), addr)
         
         except socket.timeout:
             break
 
-    print(f"loss: {100 - cnt}")   
+    print(f"loss: {100 - cnt}")   # calculate loss
         
 start_server()       
